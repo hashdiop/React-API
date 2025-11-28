9eb3de669687112ab7c65b7bc4593c46cc2e9853
hashdiop <hashdiop97@gmail.com>
2025-11-28 12:15:10 +0000

    Ajout d ela fonction Axios qui remplace le fetch

--- src/components/userslist.js
+++ src/components/userslist.js
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import Usercard from './usercard';
+import axios from 'axios';
 
 export default function Userslist() {
     const [usersdata,setUsersdata] = useState([]) 
     const [error, setError] = useState('');
 
 	 useEffect(() =>  {
-	   function fetchData() {// la fonction pour récupérer les données de l'api
-		 fetch("https://jsonplaceholder.typicode.com/users/")
-		   .then(res =>  res.json())
-		   .then(res =>  setUsersdata(res))
-		   .catch(err =>  setError(err)) ;
-	   }
+	   function fetchData() {// la fonction pour récupérer les données de l'api Avec fetch
+		//  fetch("https://jsonplaceholder.typicode.com/users/")
+    // .then(res =>  res.json())
+    // .then(res =>  setUsersdata(res))
+    // .catch(err =>  {
+    // setError(err)
+    // console.log(error)}) ;
+    
+
+    // Cas d'utilisation avec la fonction axios que l'on doit d'abord installer 
+    axios.get('https://jsonplaceholder.typicode.com/users/')
+    .then(function (response) {
+	   // gère le succès
+	   setUsersdata(response.data) ;
+	 })
+  }
 
 	   fetchData() 
 	 }, []) 
-     console.log(usersdata.forEach(user => console.log(user.title)))
+     console.log(usersdata.forEach(user => console.log(user.name)))
  return (
   <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-100 p-4">
     <div className="max-w-7xl mx-auto">
